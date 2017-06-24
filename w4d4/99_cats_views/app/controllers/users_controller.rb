eb00b647a33a9d1a24c427984bd6283513a9363d
adam <amartenj@gmail.com>
2017-06-23 21:00:58 -0700

    Complete HW for polymorphism and concerns

--- w4d4/99_cats_views/app/controllers/users_controller.rb
+++ w4d4/99_cats_views/app/controllers/users_controller.rb
@@ -4,10 +4,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      login_user!(@user)
+
       email = UserMailer.welcome_email(@user)
       email.deliver_now
 
-      login_user!(@user)
       redirect_to cats_url
     else
       flash.now[:errors] = @user.errors.full_messages
