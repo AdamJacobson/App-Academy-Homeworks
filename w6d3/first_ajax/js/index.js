f54be5dc519a95151b74aed2126779541fe09587
adam <amartenj@gmail.com>
2017-07-05 21:21:49 -0700

    W6D4 hw

--- w6d3/first_ajax/js/index.js
+++ w6d3/first_ajax/js/index.js
@@ -1,5 +1,13 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+  method: 'GET',
+  url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+  success: function(response) {
+    console.log(response.weather[0].description);
+  }
+});
 
 // Add another console log here, outside your AJAX request
+console.log("End of file");
